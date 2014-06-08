a9abca749b74ca873601c2aff1d2694c427bcc15
Arnaud DEMARCQ <arnaud.demarcq@laposte.net>
2014-06-08 10:52:06 +0200

    Tries to call an url with the HtmlUnitDriver

--- MavenSeleniumTest/src/test/java/org/krohm/test/selenium/GoogleTest.java
+++ MavenSeleniumTest/src/test/java/org/krohm/test/selenium/GoogleTest.java
@@ -7,19 +7,38 @@ package org.krohm.test.selenium;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 /**
  *
  * @author Arnaud
  */
 public class GoogleTest {
-    
-      private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GoogleTest.class);
-      
-      
-      @Test
-      public void helloWorldTest(){
-          LOGGER.debug("Hello World.");
-      }
- 
-    
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GoogleTest.class);
+
+    @Test
+    public void helloWorldTest() {
+        LOGGER.debug("Hello World.");
+    }
+
+    @Test
+    public void openBrowser() {
+        LOGGER.info("Getting base URL ...");
+        String baseUrl = System.getProperty("test.google.url");
+        LOGGER.info("base URL is: " + baseUrl);
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+    }
 }
