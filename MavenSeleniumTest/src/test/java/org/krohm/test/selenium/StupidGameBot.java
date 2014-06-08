be194c9d5af6ec5e47a77eae95b05522604e7310
Arnaud DEMARCQ <arnaud.demarcq@laposte.net>
2014-06-08 11:24:08 +0200

    Adds a sample bot for a stupid game

---
+++ MavenSeleniumTest/src/test/java/org/krohm/test/selenium/StupidGameBot.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.krohm.test.selenium;
+
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class StupidGameBot {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(StupidGameBot.class);
+    private static final String TMP_TEST_URL = "http://www.sexgangsters.com/login/";
+
+
+    @Test
+    public void harvestCash() {
+        LOGGER.info("Getting base URL ...");
+        String baseUrl = TMP_TEST_URL; //System.getProperty("test.google.url");
+        LOGGER.info("base URL is: <" + TMP_TEST_URL + ">");
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get(baseUrl);
+        LOGGER.info("Page loaded, title is: " + driver.getTitle());
+    }
+}
