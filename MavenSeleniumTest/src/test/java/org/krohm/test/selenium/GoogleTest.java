e5dad594c2a8e693b9ae276e4c9dc38f7e0f288f
Arnaud DEMARCQ <arnaud.demarcq@laposte.net>
2014-06-08 10:09:28 +0200

    Hello world test template

---
+++ MavenSeleniumTest/src/test/java/org/krohm/test/selenium/GoogleTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.krohm.test.selenium;
+
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class GoogleTest {
+    
+      private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GoogleTest.class);
+      
+      
+      @Test
+      public void helloWorldTest(){
+          LOGGER.debug("Hello World.");
+      }
+ 
+    
+}
